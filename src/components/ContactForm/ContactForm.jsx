3b144f96b4edca9b42da539bbf3012bc94daf423
alicjapol <alicjaxpoltorak@gmail.com>
2024-01-26 19:40:28 +0100

    c

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ContactForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <button type="submit">Sign up as {login}</button>
+      </form>
+    );
+  }
+}
