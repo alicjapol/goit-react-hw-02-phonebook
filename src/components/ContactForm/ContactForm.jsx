d94feb56386badd386d427d253ae412927eba1d7
alicjapol <alicjaxpoltorak@gmail.com>
2024-01-27 21:44:42 +0100

    format

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -72,7 +72,7 @@ export default class ContactForm extends Component {
               required
             />
           </label>
-         
+      
           <button type="submit">Add contact</button>
         </form>
       </div>
