3ad853659d649e420b633cc5b12069b672e915e8
alicjapol <alicjaxpoltorak@gmail.com>
2024-01-27 20:41:24 +0100

    two uls

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,23 +1,82 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
 
 export default class ContactForm extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+    filter: '',
+  };
+
+  loginInputId = nanoid();
+
+  handleChange = (evt) => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  
+    const isDuplicate = this.state.contacts.some(
+      (contact) =>
+        contact.name.toLowerCase() === this.state.name.toLowerCase() ||
+        contact.number === this.state.number
+    );
+  
+    if (isDuplicate === true) {
+
+      alert('Contact with the same name or number already exists!');
+      return;
+    }
+  };
+  
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-
-        <button type="submit">Sign up as {login}</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h2>Phonebook</h2>
+          <label>
+            Name
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              value={this.state.name}
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              onChange={this.handleChange}
+              value={this.state.number}
+              type="tel"
+              name="number"
+              required
+            />
+          </label>
+         
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
     );
   }
 }
